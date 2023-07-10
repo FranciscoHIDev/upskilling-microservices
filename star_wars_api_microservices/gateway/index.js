0ccdd2f5a4b767d499daee5cf7c47cf614692fb9
FranciscoHIDev <ihilario00@gmail.com>
2023-07-10 00:11:27 -0500

    se crea el microservicio de base de datos

--- star_wars_api_microservices/gateway/index.js
+++ star_wars_api_microservices/gateway/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(morgan("dev"))
 
 app.use("/characters", createProxyMiddleware({
-    target: "http://character:8001",
+    target: "http://characters:8001",
     changeOrigin: true
 }))
 
@@ -18,6 +18,11 @@ app.use("/planets", createProxyMiddleware({
 app.use("/films", createProxyMiddleware({
     target: "http://films:8003",
     changeOrigin: true
+}))
+
+app.use("/database",createProxyMiddleware({
+    target:"http://database:8004",
+    changeOrigin:true
 }))
 
 app.listen(8000, () => {
